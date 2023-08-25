3e181893ed3da884490963eae8d18d7e8519b8df
micah-roberson <miwirobe@iu.edu>
2023-08-24 20:18:31 -0400

    website up
    mvp

---
+++ scripts.js
@@ -0,0 +1,42 @@
+// Get all elements with the class "experience-tab" and "experience-tab-content"
+const tabs = document.querySelectorAll('.experience-tab');
+const tabContents = document.querySelectorAll('.experience-tab-content');
+
+// Function to switch between tabs and content
+function openExperience(event, experienceName) {
+    // Remove "active" class from all tabs and content
+    tabs.forEach(tab => tab.classList.remove('active'));
+    tabContents.forEach(content => content.classList.remove('active'));
+
+    // Add "active" class to the clicked tab
+    event.currentTarget.classList.add('active');
+
+    // Show the corresponding content
+    const contentToShow = document.getElementById(experienceName);
+    contentToShow.classList.add('active');
+}
+
+// Attach click event listeners to each tab
+tabs.forEach(tab => {
+    tab.addEventListener('click', (event) => {
+        openExperience(event, tab.dataset.target);
+    });
+});
+
+
+
+// Add an event listener to toggle the "sticky" class on scroll
+window.addEventListener('scroll', () => {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 0) {
+        navbar.classList.add('sticky');
+    } else {
+        navbar.classList.remove('sticky');
+    }
+});
+
+document.querySelector('.navbar-mobile').addEventListener('click', function () {
+    const navbarLinks = document.querySelector('.navbar-links');
+    navbarLinks.classList.toggle('show');
+    this.classList.toggle('active');
+});
